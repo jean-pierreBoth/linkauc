f4f2bf3ececdb8ad9a9af774efc4b7d2a4c6e25f
jean-pierreBoth <jeanpierre.both@gmail.com>
2024-06-02 16:41:01 +0200

    degree ... = removed

--- Degrees/e_vmpcr.jl
+++ Degrees/e_vmpcr.jl
@@ -1,65 +1,44 @@
 using DataFrames
 using CSV
 using Printf
+using Distributions
 
-# This function returns expected vmpcr@k given degrees quantiles in a csv file as extracted from graphembed.
+
+# This function returns expected number of nodes with no edges deleted with graph degrees quantiles in a csv file as extracted from graphembed.
 # (Cf https://github.com/jean-pierreBoth/graphembed)
 #
-# It computes for each node expected number of deleted edges and suppose they are all found up to k
-# It is thus an expected result for a perfect embedding.
-#
-# k = the number at which we compute vmpcr@k
-# p = deletion edge probability, usually 0.2
-#
-# nbsigma is to take into account incertitude around mean number of edge removed. default is 0
-# but we can compute nb removed as d*p + nbsigma * sigma. 
-# sigma in our case is sqrt(p * (1. - p) * degree)
 #
 # Usage:
 #  julia> in Julia interpreter :
 # include("e_vmpcr.jl")
-# julia> e_vmcpr("amazon-degrees.csv", 10, 0.2)
-# we get 0.263
-# julia> e_vmcpr("amazon-degrees.csv", 10, 0.2, 1.)
-# we get 0.6908
-# So even if in each quantile the number of removed edges is at 1 sigma
-# above mean, the expected vmpcr is under 0.691
+# 
+# Then for an edge deletion of probability 0.2 we get 
+# julia> removed("amazon-degrees.csv", 0.2)
+# proba node zero edge removed : 0.389
 # 
 # For the blog Catalog graph we get 
-# julia> e_vmcpr("blog-degree.csv", 10, 0.2)
-# we get  0.566
-# julia> e_vmcpr("blog-degree.csv", 10, 0.2, 1.)
-# we get  0.74
-#
-function e_vmcpr(degreefile, k, p, nbsigma::Float64=0.0)
-    #
+#julia> removed("blog-degree.csv", 0.2)
+# proba node zero edge removed : 0.120 
+
+
+# compute number of nodes with null removed edges
+function removed(degreefile,p)
     @assert p < 1.0
     # read csv file 
     df = CSV.read(degreefile, DataFrame)
     #
-    mean_degree = 0
     degrees = df[:, 2]
     quants = df[:, 1]
-    esp = 0.0
+    proba_zero = 0.
     nbslot = size(df)[1]
-    esp += p * quants[1] * degrees[1]
-    mean_degree += quants[1] * degrees[1]
-    for i in 2:nbslot
-        proba = quants[i] - quants[i-1]
-        di = degrees[i]
-        mean_degree += proba * di
-        sigma = sqrt(p * (1.0 - p) * di)
-        # removed edge is compute as mean + nbsigma sigma to get upper bounds
-        removed = min(di, p * di + sigma * nbsigma)
-        di_remain = di - removed
-        if di_remain <= k
-            esp += proba *  min(k, removed) / di_remain
-        else
-            esp += proba * min(k, removed) / k
-        end
+    b = Binomial(degrees[1], p)
+    q = cdf(b,0)
+    proba_zero += quants[1]
+    for i in 2:nbslot 
+        b = Binomial(degrees[i], p)
+        q = cdf(b,0)
+        proba_zero += q * (quants[i] - quants[i-1])
     end
-    @printf("mean degree : %.3f", mean_degree)
-    println()
-    @printf("expected  vmpcr(under mean removed number of edges) : %.3f \n", esp)
-    return esp
+    @printf("proba node zero edge removed : %.3f \n", proba_zero)    
+    return proba_zero
 end
